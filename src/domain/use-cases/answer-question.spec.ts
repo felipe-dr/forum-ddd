581ab6a718fdbb21f35a5a5bbb6014b90b5e6c11
Felipe DR <felipe.corp7@gmail.com>
2025-08-11 18:46:07 -0300

    feat: class-01 externals dependencies

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,11 +1,19 @@
 import { expect, test } from 'vitest'
 
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
-test('Create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return
+  },
+}
 
-  const answer = answerQuestion.execute({
+test('Create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
     content: 'Nova resposta',
