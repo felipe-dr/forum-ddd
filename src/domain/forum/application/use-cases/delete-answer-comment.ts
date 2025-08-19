34b3e8821133f50e4fe13830c0008ed08d907429
Felipe DR <felipe.corp7@gmail.com>
2025-08-19 20:34:15 -0300

    feat: class-03 creating generic errors

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -2,12 +2,18 @@ import { Either, left, right } from '@/core/either'
 
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 
+import { NotAllowedError } from './errors/not-allowed-error'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
 interface DeleteAnswerCommentUseCaseRequest {
   authorId: string
   answerCommentId: string
 }
 
-type DeleteAnswerCommentUseCaseResponse = Either<string, {}>
+type DeleteAnswerCommentUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {}
+>
 
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
@@ -21,11 +27,11 @@ export class DeleteAnswerCommentUseCase {
     )
 
     if (!answerComment) {
-      return left('Answer comment not found.')
+      return left(new ResourceNotFoundError())
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      return left('Not allowed.')
+      return left(new NotAllowedError())
     }
 
     await this.answerCommentsRepository.delete(answerComment)
